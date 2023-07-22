077cf9be8555f8781c0d2a5eb8748b3114d61c7c
ChavanDr99 <chavandipesh399@gmail.com>
2023-07-22 23:27:04 +0530

    dgfg

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { BrowserRouter ,Route,Routes } from 'react-router-dom'
+import React from 'react'
+import Header from './component/Header'
+import Home from './component/Home'
+import Footer from './component/Footer'
+import Video from './component/video'
+import Upload from './component/upload';
+import Login from './component/login'
+import Signup from './component/signup'
 
-function App() {
+const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
+    <div>
+
+      <BrowserRouter>
+<Header/>
+        <Routes>
+          
+<Route  path='/' element={<Home/>}/>
+<Route  path='/videos' element={<Video/>}/>
+<Route  path='/upload' element={<Upload/>}/>
+<Route  path='/signup' element={<Signup/>}/>
+<Route  path='/login' element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
